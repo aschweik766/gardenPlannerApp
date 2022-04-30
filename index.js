54d36b0ac6d9e72a7ff2a4539336bb6c4aae4616
Aschweik766 <ashnjackson766@gmail.com>
2022-04-29 22:03:28 -0600

    ready for heroku deploy attempt #1

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use('/gardenPlanner/variety',varietyControllers);
 //test app route://
 // app.get('/test', (req, res)=> {res.send('hello-test')});
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3000
 
 app.listen(port, ()=> {
     console.log(`server running on port: ${port}`)
